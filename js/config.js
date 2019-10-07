cc9f6e5394cf3798268f6a044a50a52bfd303c26
gezza-b <bachlmayr@gmail.com>
2019-10-07 10:48:57 -0400

    changed region

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_6jkvOro7l', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: 'g513i548o449namud57e5rusr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
