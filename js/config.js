38b97172f705a4c5629c5473b379ffabd87f321b
gezza-b <bachlmayr@gmail.com>
2019-10-07 10:48:10 -0400

    updated cognito settings

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_6jkvOro7l', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'g513i548o449namud57e5rusr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
